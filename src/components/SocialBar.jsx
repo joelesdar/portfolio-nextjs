38770d46bb6a124809854463b1439b8b28d9f5fc
Joel Esteban Diaz Arévalo <joel-e11@hotmail.com>
2022-02-21 14:15:07 -0500

    Social bar agregada y funcionando

--- src/components/SocialBar.jsx
+++ src/components/SocialBar.jsx
@@ -3,21 +3,64 @@ import Styled from "@emotion/styled";
 import { useTranslation } from "react-i18next";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import social from "./icons/constants";
 
 const StyledSocialBar = Styled.div`
+  /* width: 80px; */
   position: fixed;
-  /* float: right; */
-  /* right: 20%; */
+  float: right;
+  top: 45%;
+  right: 0;
+  z-index: 1;
   padding: 5px;
   background: var(--dark-blue);
-`;
+  opacity: .75;
+  backdrop-filter: blur(4px);
+  overflow-x: hidden;
+  border-radius: 5px;
+  
+  .social-icon {
+    display: inline-block;
+    width: 100%;
+    padding: 5px 0;
+  }
+  
+  .social-icon:hover {
+    color: white;
+  }
+  `;
 
 const SocialBar = () => {
+  // const instagram = COLORS.instagram;
   return (
     <StyledSocialBar>
-        <FontAwesomeIcon icon={['fab', 'github']} color='var(--green-blue)' />
-        <FontAwesomeIcon icon={['fab', 'linkedin-in']} color='var(--green-blue)' />
-        <FontAwesomeIcon icon={['fab', 'instagram']} color='var(--green-blue)' />
+      <a href={social.github}>
+        <FontAwesomeIcon
+          icon={["fab", "github"]}
+          className="social-icon"
+          color="var(--green-blue)"
+          size="lg"
+          title="GitHub"
+        />
+      </a>
+      <a href={social.linkedin}>
+        <FontAwesomeIcon
+          icon={["fab", "linkedin-in"]}
+          className="social-icon"
+          color="var(--green-blue)"
+          size="lg"
+          title="LinkedIn"
+        />
+      </a>
+      <a href={social.instagram}>
+        <FontAwesomeIcon
+          icon={["fab", "instagram"]}
+          className="social-icon"
+          color="var(--green-blue)"
+          size="lg"
+          title="Instagram"
+        />
+      </a>
     </StyledSocialBar>
   );
 };
--- src/components/icons/constants.js
+++ src/components/icons/constants.js
@@ -1,13 +1,7 @@
-const COLORS = {
-    mainBlue: 'black',
-    darkBlue: '#061C36',
-    primary: 'rebeccapurple',
+export const social = {
+    github: 'https://github.com/joelesdar',
+    linkedin: 'https://www.linkedin.com/in/joel-esteban-diaz-ar%C3%A9valo-435aab1a9/',
+    instagram: 'https://www.instagram.com/joelesdar/'
   };
-  
-  const SIZES = [
-    8,
-    16,
-    24,
-    32,
-    /* And so on */
-  ];
+
+export default social;
