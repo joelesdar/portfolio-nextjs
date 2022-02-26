4fc41b1fd9b8868f11a367874dca8ac660acb35c
Joel Esteban Diaz Arévalo <joel-e11@hotmail.com>
2022-02-26 16:32:46 -0500

    list group para la parte de experiencia agregado

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,6 +10,14 @@ const StyledExperience = Styled.div`
 padding: 48px 0;
 background: var(--black);
 
+.list-group {
+  /* background: rgba(0,0,0,0); */
+}
+
+.list-group-item {
+  /* background: rgba(0,0,0,0); */
+}
+
 `;
 
 const Experience = () => {
@@ -21,6 +29,24 @@ const Experience = () => {
         <div className="row align-items-center justify-content-around">
           <div className="col-10">
             <h2>{t("title-experience")}</h2>
+            <div class="row">
+              <div class="col-4 col-md-2">
+                <div class="list-group" id="list-tab" role="tablist">
+                  <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
+                  <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
+                  <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
+                  <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
+                </div>
+              </div>
+              <div class="col-8 colmd-10">
+                <div class="tab-content" id="nav-tabContent">
+                  <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">...</div>
+                  <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">...</div>
+                  <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
+                  <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
