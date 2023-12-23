85cfaa3aa4cd12842275670dfea4a3f592431024
Joel Esteban Diaz Arévalo <joel-e11@hotmail.com>
2023-12-23 00:17:25 -0500

    Versión de node corregida

--- pages/_app.js
+++ pages/_app.js
@@ -3,21 +3,10 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { appWithTranslation } from 'next-i18next'
-import { useEffect } from 'react'
-import { initGA, logPageView } from '../analytics'
 
 config.autoAddCss = false
 
 const MyApp = ({ Component, pageProps }) => {
-
-  useEffect(() => {
-    if (!window.GA_INITIALIZED) {
-      initGA();
-      window.GA_INITIALIZED = true;
-    }
-    logPageView();
-  }, [])
-
   return <Component {...pageProps} />
 }
 
