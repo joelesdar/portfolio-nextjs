a7ff95ee9e1f1885aa0abebd7ded46755e89ebc3
Joel Esteban Diaz Arévalo <joel-e11@hotmail.com>
2023-12-23 19:39:55 -0500

    Carpeta Profile eliminada

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -36,8 +36,8 @@ export const Header = () => {
             </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
               <Nav className="navbar-nav">
-                <Nav.Link className='active' href={`/${i18n.language}/profile`} locale={i18n.language}>{t('link-profile')}</Nav.Link>
-                <Nav.Link href='Profile.pdf' download="Profile.pdf">{t('download-cv')}</Nav.Link>
+                <Nav.Link className='active' href={`/${i18n.language}/Profile`} locale={i18n.language}>{t('link-profile')}</Nav.Link>
+                <Nav.Link href='profile.pdf' download="Profile.pdf">{t('download-cv')}</Nav.Link>
                 <NavDropdown title={t('selectLanguage')} id="basic-nav-dropdown">
                   <NavDropdown.Item>
                     <Link href={''} locale={'es'}>
