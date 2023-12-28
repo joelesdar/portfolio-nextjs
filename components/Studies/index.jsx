5ed9ca9c2583b21a0cc7252e811bb070a5e5ee12
Joel Esteban Diaz Arévalo <joel-e11@hotmail.com>
2023-12-28 16:50:47 -0500

    Componente de estudios actualizado y error de las animaciones de los logos en movil corregido

--- components/Studies/index.jsx
+++ components/Studies/index.jsx
@@ -31,6 +31,17 @@ export const Studies = () => {
                 <h4 className='career-title'>{t('title-career')}</h4>
                 <p>{t('description-university')}</p>
               </motion.div>
+              <motion.div
+                className="col-12 d-md-none"
+                initial={{ opacity: 0, x: '-100%'}}
+                whileInView={{ opacity: 100, x: 0}}
+                viewport={{ once: true }}
+                transition={{duration: 1}}>
+                <h5 className='institution'>{t('title-university')}</h5>
+                <div className='logo'>
+                  <Image className='logo-university' src={unal} alt='Universidad Nacional de Colombia' title='Universidad Nacional de Colombia' />
+                </div>
+              </motion.div>
               <motion.div 
                 className='study-block col-12 col-md-6'
                 initial={{ opacity: 0, x: '100%'}}
@@ -38,10 +49,22 @@ export const Studies = () => {
                 viewport={{ once: true }}
                 transition={{duration: 1}}>
                 <h4 className='career-title'>{t('title-career-platzi')}</h4>
-                <p>{t('description-platzi')}</p>
+                <p>{t('description-platzi')}<a href="https://platzi.com/p/joelesdar/" target='_blank'>Platzi</a>.</p>
+                
+              </motion.div>
+              <motion.div
+                className="col-12 d-md-none"
+                initial={{ opacity: 0, x: '100%'}}
+                whileInView={{ opacity: 100, x: 0}}
+                viewport={{ once: true }}
+                transition={{duration: 1}}>
+                <h5 className='institution'>Platzi</h5>
+                <div className='logo'>
+                  <Image className='logo-university' src={platzi} alt='Platzi' title='Platzi' />
+                </div>
               </motion.div>
             </div>
-            <div className="row">
+            <div className="row d-none d-md-flex">
               <motion.div
                 className="col-6"
                 initial={{ opacity: 0, x: '-100%'}}
