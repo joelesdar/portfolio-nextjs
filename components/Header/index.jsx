94c38ae943126a3db3fcbf3306ab35729edf9f44
Joel Esteban Diaz Arévalo <joel-e11@hotmail.com>
2024-01-15 22:56:53 -0500

    Problema boton de inicio en el header solucionado

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -36,7 +36,7 @@ export const Header = () => {
             </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
               <Nav className="navbar-nav">
-                <Nav.Link href='/'>{t('home')}</Nav.Link>
+                <Nav.Link href={`/${i18n.language}`}>{t('home')}</Nav.Link>
                 <Nav.Link className='active' href={`/${i18n.language}/profile`} locale={i18n.language}>{t('link-profile')}</Nav.Link>
                 {/* <Nav.Link href='profile.pdf' download="Profile.pdf">{t('download-cv')}</Nav.Link> */}
                 <NavDropdown title={t('selectLanguage')} id="basic-nav-dropdown">
